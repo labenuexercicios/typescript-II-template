8500977074dbb60e8620f41e3b466f1ec5f98af1
milenarismo <milenasimoeslabenu@gmail.com>
2023-01-04 11:02:04 -0300

    Finalizado

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,28 @@ Crie um sistema de cadastro de usuários que contenha:
   b. NormalAccount
 */
 
+//1.1
+type TPerson = {
+  id: string,
+  name: string,
+  email: string,
+  password: string
+  role: "Normal" | "Admin"
+}
 
 
 
+
+//1.2
+type AdminAccount = {
+  account: string | number,
+  permission: boolean
+}
+
+type NormalAccount = {
+  account: string | number,
+  permission: boolean
+}
 
 
 
@@ -32,3 +51,52 @@ Vamos continuar nosso sistema de cadastro de usuários criando:
 5. Guarde essas pessoas no array de usuários.
 
 */ 
+
+enum Role  {
+ ADMIN = "Admin",
+ NORMAL = "Normal"
+}
+
+const userOne: TPerson = {
+  id:"01",
+  name:"Milena",
+  email:"milena@email.com",
+  password: "123456",
+  role: Role.ADMIN
+}
+
+//2.2
+type AdminUser = TPerson & AdminAccount
+
+const userAdmin: AdminUser = {
+  id: "03",
+  name: "Milena",
+  email: "milena@email.com",
+  password: "654321",
+  role: Role.ADMIN,
+  account: "admin1",
+  permission: true
+
+}
+
+type NormalUser = TPerson & NormalAccount
+
+const userNormal: NormalUser = {
+  id: "04",
+  name: "Maria",
+  email: "maria@email.com",
+  password: "65432177",
+  role: Role.NORMAL,
+  account: "normal1",
+  permission: false
+}
+
+
+
+
+
+
+
+
+
+
